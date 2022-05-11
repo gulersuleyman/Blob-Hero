864195a7c0f80deb9e3e6cb82e2736c479cd472a
gulersuleyman <90650029+gulersuleyman@users.noreply.github.com>
2022-05-11 14:25:28 +0300

    p

---
+++ Blob Hero/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float _moveSpeed;
+
+
+
+    Rigidbody _rigidbody;
+    DynamicJoystick _joystick;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _joystick = FindObjectOfType<DynamicJoystick>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 direction = Vector3.forward * _joystick.Vertical + Vector3.right * _joystick.Horizontal;
+        _rigidbody.AddForce(direction * _moveSpeed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+    }
+}
