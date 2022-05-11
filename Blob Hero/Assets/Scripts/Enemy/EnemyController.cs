a278d3d3e666cff39c95d19820e717fe1799117b
gulersuleyman <90650029+gulersuleyman@users.noreply.github.com>
2022-05-11 15:47:23 +0300

    p

--- Blob Hero/Assets/Scripts/Enemy/EnemyController.cs
+++ Blob Hero/Assets/Scripts/Enemy/EnemyController.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
+
+
+    PlayerController _player;
+    NavMeshAgent _agent;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        _agent = GetComponent<NavMeshAgent>();
+        _player = FindObjectOfType<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _agent.Move(_player.transform.position);
     }
 }
