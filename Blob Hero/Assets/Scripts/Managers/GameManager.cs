8761d941aa0e9dc565c30cf04aa49e59b56ac9fc
gulersuleyman <90650029+gulersuleyman@users.noreply.github.com>
2022-05-13 11:32:15 +0300

    p

---
+++ Blob Hero/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public float levelExperience;
+    public float chapterExperience;
+
+    public static GameManager Instance { get; private set; }
+
+
+
+    private void Awake()
+    {
+        SingletonThisGameObject();
+    }
+    public void SingletonThisGameObject()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
