ccf490f77e014951b4efd7b5e54270fd53e363d7
gulersuleyman <90650029+gulersuleyman@users.noreply.github.com>
2022-05-16 16:02:48 +0300

    p

--- Blob Hero/Assets/Scripts/Skills/LightningSkill.cs
+++ Blob Hero/Assets/Scripts/Skills/LightningSkill.cs
@@ -6,9 +6,13 @@ using DG.Tweening;
 
 public class LightningSkill : MonoBehaviour
 {
+	[SerializeField] Transform firstPosition;
 
+	bool canMove;
 	private void OnEnable()
 	{
+		canMove = true;
+		transform.position = firstPosition.position;
 		StartCoroutine(ActiveFalse());
 	}
 
@@ -16,11 +20,17 @@ public class LightningSkill : MonoBehaviour
 	{
 		if (other.gameObject.CompareTag("Enemy") || other.gameObject.CompareTag("Spear"))
 		{
-			Vector3 targetPos = new Vector3(other.gameObject.transform.position.x, transform.position.y, other.gameObject.transform.position.z);
-			transform.DOMove(targetPos, 0.5f).OnComplete(() =>
+			if(canMove)
 			{
-				Destroy(other.gameObject);
-			});
+				canMove = false;
+				Vector3 targetPos = new Vector3(other.gameObject.transform.position.x, transform.position.y, other.gameObject.transform.position.z);
+				transform.DOMove(targetPos, 0.5f).OnComplete(() =>
+				{
+					Destroy(other.gameObject);
+					canMove = true;
+				});
+			}
+			
 		}
 	}
 	IEnumerator ActiveFalse()
