a59bd51cbb3c8771a780324b25dc05a1ae00a605
gulersuleyman <90650029+gulersuleyman@users.noreply.github.com>
2022-05-19 22:04:30 +0300

    p

--- Blob Hero/Assets/Scripts/UIs/LevelUpCanvas.cs
+++ Blob Hero/Assets/Scripts/UIs/LevelUpCanvas.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class LevelUpCanvas : MonoBehaviour
 {
-
+	[SerializeField] GameObject[] skills;
     [SerializeField] GameObject[] skillButtonPrefabs;
-
+	[SerializeField] Transform left, middle, right;
 	private void OnEnable()
 	{
-		
+		int buttonCount = 0;
+		for (int i = 0; i < 8; i++)
+		{
+			if(!skills[i].gameObject.GetComponent<LockControl>().unLocked && buttonCount<3)
+			{
+				skillButtonPrefabs[i].gameObject.SetActive(true);
+				if (buttonCount == 0)
+					skillButtonPrefabs[i].transform.position = left.position;
+				if (buttonCount == 1)
+					skillButtonPrefabs[i].transform.position = middle.position;
+				if (buttonCount == 2)
+					skillButtonPrefabs[i].transform.position = right.position;
+
+				buttonCount++;
+			}
+		}
 	}
 }
